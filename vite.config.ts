bb78dd9de14a98cea768ee712c82a17c7b759f55
Ushakov341 <ushakov341@gmail.com>
2026-04-05 18:37:34 +0200

    build test

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/spacex-launches-2020/',
+  base: 'https://github.com/Ushakov341/ushakov_ks_3.2.3/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
