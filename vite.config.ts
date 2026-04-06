20dbb9d2132754043d735fd71f8a2fbbc073f99c
Ushakov341 <ushakov341@gmail.com>
2026-04-06 11:01:54 +0200

    Финал

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://github.com/Ushakov341/ushakov_ks_3.2.3/',
+  base: '/ushakov_ks_3.2.3/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
